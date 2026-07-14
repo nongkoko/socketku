800d7c307aa7319d58e2bf03a8da2d4548d544c9
tukangPES <afryanto.livan@outlook.com>
2026-07-15 01:38:02 +0700

    add kemampuan listen

--- soketku.cs
+++ soketku.cs
@@ -15,6 +15,7 @@ public interface iSoketku
 {
     void connect(string ipAddress, int port);
     void send(string dataToSend);
+    Task listenAsync(int port);
     string connName { get; set; }
     iTCPheader tcpHeader { get; set; }
 }
@@ -22,10 +23,8 @@ public interface iSoketku
 internal class soketku : iSoketku
 {
     private Socket _socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-
     string iSoketku.connName { get; set; }
     iTCPheader iSoketku.tcpHeader { get; set; }
-
     void iSoketku.connect(string ipAddress, int port)
     {
         _socket.Connect(ipAddress, port);
@@ -34,10 +33,13 @@ internal class soketku : iSoketku
         Task.Run(async () =>
         {
             var thisAsiSoket = (iSoketku)this;
+            var mainBuffer = new byte[32767];
             var aBuffer = new byte[16384];
             var jumlahByteRead = 0;
             var panjangTotal = (ushort)0;
             var thisAsISocket = (iSoketku)this;
+            var writePointer = 0; //turns out writePointer itu sama dengan jumlah data yang sudah diterima
+
             while ((jumlahByteRead = await _socket.ReceiveAsync(aBuffer)) > 0)
             {
                 if (thisAsISocket.tcpHeader == null)
@@ -52,6 +54,8 @@ internal class soketku : iSoketku
                 else
                     panjangTotal = BinaryPrimitives.ReadUInt16LittleEndian(aBuffer);
 
+
+
                 if (jumlahByteRead > panjangTotal)
                 {
                     var theString = System.Text.Encoding.UTF8.GetString(aBuffer, 0, jumlahByteRead);
@@ -60,6 +64,33 @@ internal class soketku : iSoketku
 
             }
         });
+    }
+
+    private TcpListener? _tcpListener = null;
+    async Task iSoketku.listenAsync(int port)
+    {
+        var alreadyListen = _tcpListener != null;
+        if (alreadyListen)
+            return;
+
+        _tcpListener = new TcpListener(System.Net.IPAddress.Any, port);
+        _tcpListener.Start();
+        while (true)
+        {
+            var client = await _tcpListener.AcceptTcpClientAsync();
+            _ = handleClientAsync(client);
+        }
+    }
+
+    private async Task handleClientAsync(TcpClient client)
+    {
+        var stream = client.GetStream();
+        var buffer = new byte[16384];
+        int bytesRead;
+        while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+        {
+            var receivedData = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+        }
     }
 
     void iSoketku.send(string dataToSend)
@@ -72,7 +103,6 @@ internal class soketku : iSoketku
             _socket.Send(payload, 0, payload.Length, SocketFlags.None);
             return;
         }
-
 
         var buffer = new byte[5000];
         var panjangAsHeader = (ushort)payload.Length;
